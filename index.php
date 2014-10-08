fd2e292a189656fcef66d07d7868d4987a532d7b
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2014-10-08 21:22:11 +0100

    Now response with users message

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Message>Hello, Mobile Monkey</Message>
+    <Message><?php echo $_REQUEST['From']; ?></Message>
 </Response>
